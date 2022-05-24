d897c4b8518b79e1a75264a57d54393aa73a18b1
martinKindall <mart256@gmail.com>
2022-05-24 21:19:32 +0200

    Creating api gateway in http mode integrated with SQS queue

--- lib/scalable_endpoint-stack.ts
+++ lib/scalable_endpoint-stack.ts
@@ -1,7 +1,7 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import { Duration, Stack, StackProps, CfnRefElement } from 'aws-cdk-lib';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as apigatewayv2 from 'aws-cdk-lib/aws-apigatewayv2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class ScalableEndpointStack extends Stack {
@@ -12,8 +12,33 @@ export class ScalableEndpointStack extends Stack {
       visibilityTimeout: Duration.seconds(300)
     });
 
-    const topic = new sns.Topic(this, 'ScalableEndpointTopic');
+    const statement = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: [queue.queueArn],
+      actions: ['sqs:SendMessage']
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const policy = new iam.Policy(this, 'MyPolicy');
+    policy.addStatements(statement);
+
+    const role = new iam.Role(this, 'someRole', {assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com')});
+    role.attachInlinePolicy(policy);
+
+    const httpApi = new apigatewayv2.CfnApi(this, 'MyHttpApi', {
+      name: 'MyHttpApi',
+      protocolType: 'HTTP'
+    });
+
+    new apigatewayv2.CfnIntegration(this, "SqsIntegration", {
+      apiId: httpApi.ref,
+      integrationSubtype: 'SQS-SendMessage',
+      integrationType: 'AWS_PROXY',
+      payloadFormatVersion: '1.0',
+      credentialsArn: role.roleArn,
+      requestParameters: {
+        QueueUrl: queue.queueUrl,
+        MessageBody: "$request.body.message"
+      }
+    });
   }
 }
